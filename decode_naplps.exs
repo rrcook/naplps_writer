d974c290d32eeda51543132ecfec9c956c9cf1bc
Richard Cook <blubparadox@gmail.com>
2026-08-04 21:01:59 -0400

    Added header comment.

--- decode_naplps.exs
+++ decode_naplps.exs
@@ -1,3 +1,18 @@
+# Copyright 2026, Ralph Richard Cook
+#
+# This file is part of Prodigy Reloaded.
+#
+# Prodigy Reloaded is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Prodigy Reloaded is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with Prodigy Reloaded. If not,
+# see <https://www.gnu.org/licenses/>.
+
 # Coordinate decoding lives in XYUtilities (lib/xy_utilities.ex), which is
 # unit-tested. Load it from source when running standalone (`elixir
 # decode_naplps.exs ...`); under `mix run` it is already compiled and on the
