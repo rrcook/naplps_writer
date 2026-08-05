011072cd8a472a1d6a94c12c8bb724d89a21608f
Richard Cook <blubparadox@gmail.com>
2026-08-04 21:01:43 -0400

    Updated version, many changes since previous.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NaplpsWriter.MixProject do
   def project do
     [
       app: :naplps_writer,
-      version: "0.1.0",
+      version: "0.1.5",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
